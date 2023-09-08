99964435267db26320df7237a48c025f6ed427a6
Quynh Nguyen <qtn2@case.edu>
2023-09-08 09:53:21 -0400

    van page detail

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,17 @@
-
+import { Link } from "react-router-dom"
 const Card = (props) => {   
     const { id, imageUrl, name, price, type } = props
-    console.log(id, imageUrl, name, price, type)
     return (
-        <div key={id} className="van-tile">
-            <img src={imageUrl} />
-            <div className="van-info">
-                <h3>{name}</h3>
-                <p>${price}<span>/day</span></p>
+            <div key={id} className="van-tile">
+                 <Link to={`/van/${id}`}>
+                    <img src={imageUrl} />
+                    <div className="van-info">
+                        <h3>{name}</h3>
+                        <p>${price}<span>/day</span></p>
+                    </div>
+                    <i className={`van-type ${type} selected`}>{type}</i>
+                </Link>
             </div>
-            <i className={`van-type ${type} selected`}>{type}</i>
-        </div>
     )
 
 }
